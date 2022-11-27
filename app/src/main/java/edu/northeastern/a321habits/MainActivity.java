1d69ba7f6e9ec85e92a0822ff2b0341081b85096
swatiagarwal <agarwal.sw@northeastern.edu>
2022-11-26 21:57:20 -0500

    Added insights and settings icon on app action toolbar

--- app/src/main/java/edu/northeastern/a321habits/MainActivity.java
+++ app/src/main/java/edu/northeastern/a321habits/MainActivity.java
@@ -2,16 +2,16 @@ package edu.northeastern.a321habits;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
 import edu.northeastern.a321habits.ui.main.SectionsPagerAdapter;
 import edu.northeastern.a321habits.databinding.ActivityMainBinding;
@@ -19,10 +19,11 @@ import edu.northeastern.a321habits.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private int[] icons = {
+    private final int[] icons = {
             R.drawable.ic_home_black_24dp,
             R.drawable.ic_groups_black_24dp,
     };
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +43,44 @@ public class MainActivity extends AppCompatActivity {
             tabs.getTabAt(i).setIcon(icons[i]);
         }
 
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("onCreate", "menu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_item_progress) {
+
+            // Do something
+
+            Toast.makeText(this, "progress", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.action_item_settings) {
+
+            // Do something
+            Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
